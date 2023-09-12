22ac3dd5f0486ff4fbb2a61fa56a92cb2ea00176
FarisAchmad02 <farisachmad02@gmail.com>
2023-09-12 17:36:45 +0000

    Replace employee.controller.js

--- server/controllers/employee.controller.js
+++ server/controllers/employee.controller.js
@@ -1,107 +1,238 @@
 const { json } = require("body-parser");
+const moment = require('moment'); // For date formatting
 const { sequelize } = require("../models/index.js");
 
 // GET untuk menampilkan dataa
-const getEmployees = async (req, res) => {
+const get = async (req, res) => {
   try {
-    const query = 'SELECT * FROM employee;';
-    const [employees, metadata] = await sequelize.query(query, {
-      type: sequelize.QueryTypes.SELECT,
+    const param = req.query;
+    let keyword = param?.keyword || ``;
+    let limit = parseInt(param?.limit) || 5;
+    let offset = parseInt(param?.offset) || 0;
+
+    let query = `SELECT * FROM employee`;
+
+    if (keyword.length > 0) {
+      query += ` 
+        WHERE 
+          name LIKE '%${keyword}%'`;
+    }
+
+    if (limit > 0) {
+      query += " LIMIT " + limit;
+    }
+
+    if (offset >= 0) {
+      query += " OFFSET " + offset;
+    }
+
+    const [rows] = await sequelize.query(query);
+    
+
+    const count = rows.length;
+    const data = {
+      total_row: count,
+      limit: limit,
+      offset: offset,
+      rows: rows,
+    };
+
+    res.status(201).json({
+      status: true,
+      message: "GET DATA employee",
+      data: data,
     });
-    res.status(200).json(employees);
-  } catch (error) {
-    res.status(500).json({ error: 'Gagal mendapatkan Employee.' });
+  } catch (err) {
+    res.status(500).json({
+      status: false,
+      message: err.message,
+      data: [],
+    });
   }
 };
 
 //GET BY ID untuk GET single dataa
-const getEmployeeById = async (req, res) => {
-  const { id } = req.params;
+const getById = async (req, res) => {
   try {
-    const query = 'SELECT * FROM employee WHERE id_employee = :id;';
-    const [employee, metadata] = await sequelize.query(query, {
-      replacements: { id },
-      type: sequelize.QueryTypes.SELECT,
-    });
-    if (employee.length === 0) {
-      return res.status(404).json({ error: 'Employee tidak ditemukan.' });
+    const employeeId = req.params.id;
+
+    let query = `SELECT * FROM employee WHERE id_employee = $id_employee`;
+    const [employee] = await sequelize.query(query, {  
+      bind: { 
+      id_employee : employeeId
     }
-    res.status(200).json(employee[0]);
-  } catch (error) {
-    res.status(500).json({ error: 'Gagal mendapatkan Employee.' });
+      });
+  
+
+    if (employee.length > 0) {
+      res.status(200).json({
+        status: true,
+        message: 'MENDAPATKAN DATA pengguna',
+        data: employee.map((Idemployee) => ({
+          ...Idemployee,
+          last_login: moment(Idemployee.last_login).format('YYYY-MM-DD HH:mm:ss'),
+        })),
+      });
+    } else {
+      res.status(404).json({
+        status: false,
+        message: 'DATA pengguna dengan ID ' + employeeId + ' TIDAK DITEMUKAN', // Menggunakan id_users di sini
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      status: false,
+      message: err.message,
+    });
   }
 };
 
 //STORE untuk insert dataa
-const storeEmployee = async (req, res) => {
+const store = async (req, res) => {
   try {
-    const { name, agama, status, alamat, id_outlet } = req.body;
-    const query = `
-      INSERT INTO employee (name, agama, status, alamat, id_outlet, created_at, updated_at)
-      VALUES (:name, :agama, :status, :alamat, :id_outlet, NOW(), NOW());
-    `;
-    const [results, metadata] = await sequelize.query(query, {
-      replacements: {
-        name,
-        agama,
-        status,
-        alamat,
-        id_outlet,
+    const param = req.body;
+
+    const createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
+    const updatedAt = createdAt;
+    const lastLogin = createdAt;
+
+    let query = `INSERT INTO employee (
+      id_outlet, name, agama, status, alamat, created_at, updated_at
+      ) VALUES (
+      $id_outlet, $name, $agama, $status, $alamat, $created_at, $updated_at
+    )`;
+
+    const [result_id] = await sequelize.query(query, {
+      bind: {
+        id_outlet: param.id_outlet,
+        name: param.name, // Use the hashed password here
+        agama: param.agama,
+        status: param.status,
+        alamat: param.alamat,
+        created_at: createdAt,
+        updated_at: null,
       },
-      type: sequelize.QueryTypes.INSERT,
     });
-    res.status(201).json({ message: 'Employee berhasil dibuat.' });
-  } catch (error) {
-    res.status(500).json({ error: 'Gagal membuat Employee.' });
+
+    return res.status(201).json({
+      status: true,
+      message: 'ADD employee SUCCESS',
+      data: {
+        id: result_id,
+        ...param,
+        updated_at: null,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    let validationError = JSON.parse(JSON.stringify(err))?.original;
+
+    res.status(500).json({
+      status: false,
+      message: validationError?.sqlMessage || err.message,
+      data: [],
+    });
   }
 };
 
 
 //UPDATE untuk update data
-const updateEmployee = async (req, res) => {
-  const { id } = req.params;
-  const { name, agama, status, alamat, id_outlet } = req.body;
+const update = async (req, res) => {
   try {
-    const query = `
-      UPDATE employee
-      SET name = :name, agama = :agama, status = :status, alamat = :alamat, id_outlet = :id_outlet, updated_at = NOW()
-      WHERE id_employee = :id;
-    `;
-    const [updated, metadata] = await sequelize.query(query, {
-      replacements: { id, name, agama, status, alamat, id_outlet },
-      type: sequelize.QueryTypes.UPDATE,
-    });
-    if (updated[0] === 0) {
-      return res.status(404).json({ error: 'Employee tidak ditemukan.' });
+    const employeeId  = req.params.id;
+
+    const updateat = moment().format('YYYY-MM-DD HH:mm:ss');
+      
+    if (employeeId) {
+      const query = `UPDATE employee SET  
+      id_outlet = $id_outlet, 
+      name = $name, 
+      agama = $agama,
+      status = $status,
+      updated_at = $updateAt
+      WHERE id_employee = ${parseInt(employeeId)}`;
+      
+      const [result] = await sequelize.query(query, {
+        bind: {
+          id_outlet: req.body?.id_outlet,
+          name: req.body?.name,
+          agama: req.body?.agama,
+          status: req.body?.status,
+          updateAt: updateat
+        }
+      });
+
+      if (result.affectedRows > 0) {
+        res.status(200).json({
+          status: true,
+          message: 'UPDATE SUCCESS',
+          data: result,
+        });
+      } else {
+        res.status(404).json({
+          status: false,
+          message: 'UPDATE FAILED',
+        });
+      }
+    } else {
+      res.status(500).json({
+        status: false,
+        message: `NO USER WITH ID ${userId}`,
+      });
     }
-    res.status(200).json({ message: 'Employee berhasil diperbarui.' });
-  } catch (error) {
-    res.status(500).json({ error: 'Gagal memperbarui Employee.' });
+  } catch (err) {
+    console.log(err);
+    let validationError = JSON.parse(JSON.stringify(err))?.original;
+
+    res.status(500).json({
+      status: false,
+      message: validationError?.sqlMessage || err.message,
+    });
   }
-};
+}
 
 //DESTROY untuk delete data
-const deleteEmployee = async (req, res) => {
-  const { id } = req.params;
+const destroy = async (req, res) => {
   try {
-    const query = 'DELETE FROM employee WHERE id_employee = :id;';
-    const [deleted, metadata] = await sequelize.query(query, {
-      replacements: { id },
-      type: sequelize.QueryTypes.DELETE,
-    });
-    if (deleted === 0) {
-      return res.status(404).json({ error: 'Employee tidak ditemukan.' });
+    const employeeId  = req.params.id;
+
+    if (employeeId) {
+      const query = `DELETE FROM employee WHERE id_employee = ${employeeId}`;
+      const [result] = await sequelize.query(query);
+
+      if (result?.affectedRows > 0) {
+        res.status(200).json({
+          status: true,
+          message: 'SUCCESS DELETE DATA employee',
+        });
+      } else {
+        res.status(404).json({
+          status: false,
+          message: `NO USER WITH ID ${employeeId}`,
+        });
+      }
+    } else {
+      res.status(500).json({
+        status: false,
+        message: 'INVALID USER ID',
+      });
     }
-    res.status(204).send();
-  } catch (error) {
-    res.status(500).json({ error: 'Gagal menghapus Employee.' });
+  } catch (err) {
+    console.log(err);
+    let validationError = JSON.parse(JSON.stringify(err))?.original;
+
+    res.status(500).json({
+      status: false,
+      message: validationError?.sqlMessage || err.message,
+    });
   }
 };
 
 module.exports = {
-  getEmployees,
-  getEmployeeById,
-  storeEmployee,
-  updateEmployee,
-  deleteEmployee,
+  get,
+  getById,
+  store,
+  update,
+  destroy,
 };
