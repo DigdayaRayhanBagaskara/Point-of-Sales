acf7be4e8579395df99876ee935a401974e66e2b
FarisAchmad02 <farisachmad02@gmail.com>
2023-09-12 17:35:04 +0000

    Replace users.route.js

--- server/routes/users.route.js
+++ server/routes/users.route.js
@@ -2,31 +2,28 @@ const express = require('express');
 const usersController = require('../controllers/users.controller.js');
 const router = express.Router();
 // const authMiddleware = require('../middleware/authMiddleware').default;
-const protect = require('../middleware/auth.js');
-
-//Login dan Logout
-router.post('/login', usersController.login);
-router.post('/logout', usersController.logout);
-
-// Rute yang hanya bisa diakses oleh admin
-router.get('/admin', protect, usersController.adminRoute);
-
-// Rute yang bisa diakses oleh user dan admin
-router.get(
-  '/userRole', usersController.cashierRoute
-);
+const { protect } = require('../middleware/auth.js');
+const { route } = require('./employee.route.js');
 
 //Get
+//router.get('/Role/:id', usersController.getByIdRole);
+//router.get('/Role', usersController.showRole);
 router.get('/:id', usersController.getById);
 router.get('/', protect, usersController.get);
 
 //Inser
+//router.post('/Role/',usersController.storeRole);
 router.post('/', usersController.store);
 
+
 //Update
+//router.put('/Role/:id', usersController.updateRole);
 router.put('/:id', protect, usersController.update);
+
 
 //Delete
 router.delete('/:id', usersController.destroy);
+//router.delete('/Role/:id', usersController.deleteRole);
+
 
 module.exports = router;
