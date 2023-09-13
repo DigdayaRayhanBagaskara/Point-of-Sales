3f3022316790f44c48068db16de724d1b067ef34
TaufiqHidayah6 <taufiqhidayah0611@gmail.com>
2023-09-14 05:23:09 +0700

    Perbaikan Pada Table, Tampil Gambar

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -1,25 +1,26 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import { MagnifyingGlassCircleIcon } from "@heroicons/react/24/solid";
-import { Typography, CardBody, Input } from "@material-tailwind/react";
-import { useState } from "react";
+
+import { Typography, CardBody } from "@material-tailwind/react";
+
 const Table = ({ head, rows }) => {
   return (
     <>
-      <CardBody className="overflow-scroll px-0">
+      <CardBody className="h-full w-full ">
         <table className=" mt-4 w-full min-w-max table-auto text-left">
           {/* HEADER TABLE */}
+
           <thead className="border-blue-gray-500">
             <tr className="text-center">
               {head.map((col, index_col) => (
                 <th
                   key={index_col}
-                  className="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4"
+                  className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
                 >
                   <Typography
                     variant="small"
                     color="blue-gray"
-                    className="font-normal leading-none opacity-70"
+                    className="font-normal leading-none opacity-70 "
                   >
                     {col.name}
                   </Typography>
@@ -38,16 +39,29 @@ const Table = ({ head, rows }) => {
               return (
                 <tr key={index} className="text-center">
                   {head.map((col, index_col) => (
-                    <td className={classes} key={index_col}>
-                      <div className="flex flex-col">
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal opacity-70"
-                        >
-                          {col.cell(row) || "-"}
-                        </Typography>
-                      </div>
+                    <td
+                      className={`${classes} bg-blue-gray-50/50 w-16`}
+                      key={index_col}
+                    >
+                      {col.name === "GAMBAR PRODUK" ? (
+                        <div className="flex flex-col items-center justify-center">
+                          <img
+                            src={col.cell(row)}
+                            alt="Image"
+                            className="w-16 h-16 rounded-full mx-auto"
+                          />
+                        </div>
+                      ) : (
+                        <div className="flex flex-col">
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal opacity-70"
+                          >
+                            {col.cell(row) || "-"}
+                          </Typography>
+                        </div>
+                      )}
                     </td>
                   ))}
                 </tr>
