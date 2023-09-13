08789747aad054e370f255b89d1583167a628e80
TaufiqHidayah6 <taufiqhidayah0611@gmail.com>
2023-09-14 05:21:47 +0700

    Perbaikan Pagination

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { useState } from "react";
-import { Button, IconButton } from "@material-tailwind/react";
+import { Button, CardFooter, IconButton } from "@material-tailwind/react";
 import { ArrowRightIcon, ArrowLeftIcon } from "@heroicons/react/24/outline";
 
 const Pagination = ({ totalPages, onPageChange }) => {
   const [active, setActive] = useState(1);
+  let [startnumber, endnumber] = useState(0);
 
   const getItemProps = (index) => ({
     variant: active === index ? "filled" : "text",
@@ -32,7 +33,19 @@ const Pagination = ({ totalPages, onPageChange }) => {
 
   const generatePageButtons = () => {
     const buttons = [];
-    for (let i = 1; i <= totalPages; i++) {
+
+    if (active > 2) {
+      startnumber = active - 2;
+    } else {
+      startnumber = 1;
+    }
+    if (active < totalPages - 2) {
+      endnumber = active + 2;
+    } else {
+      endnumber = totalPages;
+    }
+
+    for (let i = startnumber; i <= endnumber; i++) {
       buttons.push(
         <IconButton className="rounded-full" key={i} {...getItemProps(i)}>
           {i}
@@ -43,27 +56,39 @@ const Pagination = ({ totalPages, onPageChange }) => {
   };
 
   return (
-    <div className="flex items-center gap-4">
-      <Button
-        variant="text"
-        className="flex items-center gap-2 rounded-full"
-        onClick={prev}
-        disabled={active === 1}
-      >
-        <ArrowLeftIcon strokeWidth={2} className="h-4 w-4" />
-        Previous
-      </Button>
-      <div className="flex items-center gap-2">{generatePageButtons()}</div>
-      <Button
-        variant="text"
-        className="flex items-center gap-2 rounded-full"
-        onClick={next}
-        disabled={active === totalPages}
-      >
-        Next
-        <ArrowRightIcon strokeWidth={2} className="h-4 w-4" />
-      </Button>
-    </div>
+    <>
+      <div className="mx-7 my-4 justify-center items-center">
+        <span className="text-gray-500 mr-2">
+          Page {active} of {totalPages}
+        </span>
+      </div>
+
+      <CardFooter className="flex items-center justify-center border-t border-blue-gray-50 p-4">
+        <div className="flex items-center gap-4">
+          <Button
+            variant="text"
+            className="flex items-center gap-2 rounded-full"
+            onClick={prev}
+            disabled={active === 1}
+          >
+            <ArrowLeftIcon strokeWidth={2} className="h-4 w-4" />
+            Previous
+          </Button>
+
+          <div className="flex items-center gap-2">{generatePageButtons()}</div>
+
+          <Button
+            variant="text"
+            className="flex items-center gap-2 rounded-full"
+            onClick={next}
+            disabled={active === totalPages}
+          >
+            Next
+            <ArrowRightIcon strokeWidth={2} className="h-4 w-4" />
+          </Button>
+        </div>
+      </CardFooter>
+    </>
   );
 };
 
